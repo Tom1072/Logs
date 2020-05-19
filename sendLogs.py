74a28a5179a494a1490b358e71d52548bf54834c
Tom1072 <65418329+Tom1072@users.noreply.github.com>
2020-05-19 09:33:23 -0400

    fixed sendLogs

--- sendLogs.py
+++ sendLogs.py
@@ -6,20 +6,20 @@ def main():
     data_str = populateData()
     sendData(data_str)
 
-    
-
 def populateData():
     f = open("users.json","r")
     data = json.load(f)
     data_str = []
-    for i in range(100):
-        data_str.append(json.dumps(data[i]))
+    for element in data:
+        data_str.append(json.dumps({"message":element, "type":"users"}))
     return data_str
 
 def sendData(data_str):
     sock = socket(AF_INET, SOCK_DGRAM)
+    print(data_str[1])
     for i in range(5):
         sock.sendto(bytes(data_str[i],"utf8"), ('localhost', 5400))
 
 if __name__ == "__main__":
     main()
+    
