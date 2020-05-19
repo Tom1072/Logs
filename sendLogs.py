ddd03775413fda9605ea6fe639d8d83a5963fde1
Tom1072 <65418329+Tom1072@users.noreply.github.com>
2020-05-19 09:00:59 -0400

    update

--- sendLogs.py
+++ sendLogs.py
@@ -2,16 +2,24 @@ from socket import socket, AF_INET, SOCK_DGRAM
 import random
 import json
 
-sock = socket(AF_INET, SOCK_DGRAM)
-f = open("users.json","r")
-data = json.load(f)
-data_str = []
-for i in range(100):
-    data_str.append(json.dumps(data[i]))
-    print(type(data_str[i]))
+def main():
+    data_str = populateData()
+    sendData(data_str)
 
+    
 
-# for i in data:
-    # print(i)
-# for i in range (5):
-#     sock.sendto()
+def populateData():
+    f = open("users.json","r")
+    data = json.load(f)
+    data_str = []
+    for i in range(100):
+        data_str.append(json.dumps(data[i]))
+    return data_str
+
+def sendData(data_str):
+    sock = socket(AF_INET, SOCK_DGRAM)
+    for i in range(5):
+        sock.sendto(bytes(data_str[i],"utf-8"), ('localhost', 5400))
+
+if __name__ == "__main__":
+    main()
